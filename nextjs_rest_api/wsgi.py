a04858bcfe53635d953f7c7f9a07a929d7150188
tsucchiiinoko <ccya2211@icloud.com>
2022-03-01 21:23:28 +0900

    Procfileを修正

--- nextjs_rest_api/wsgi.py
+++ nextjs_rest_api/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for nextjs_rest_api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 import dj_static import Cling
 from django.core.wsgi import get_wsgi_application
